0dde36eeef01a29ef76774d918e4fd9122f05566
lipindim <lipindim@gmail.com>
2020-11-25 18:27:59 +1000

    Домашняя работа 1

---
+++ Labyrinth/Assets/Scripts/GenCell.cs
@@ -0,0 +1,11 @@
+﻿namespace Labyrinth
+{
+
+    public struct GenCell
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool Visited { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ Labyrinth/Assets/Scripts/IInteractiveObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Labyrinth
+{
+
+    public interface IInteractiveObject
+    {
+        void Interaction();
+    }
+}
+
